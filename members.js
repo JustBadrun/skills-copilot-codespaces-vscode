1e4666239b3ae605b461b79137a72c6b2c4d766b
JustBadrun <128286595+JustBadrun@users.noreply.github.com>
2025-02-16 05:05:25 +0000

    Copiltot second commit

---
+++ members.js
@@ -0,0 +1,14 @@
+function skillsMembers() {
+  return {
+    members: [
+      {
+        name: "John Doe",
+        skills: ["React", "Node.js", "MongoDB"],
+      },
+      {
+        name: "Jane Doe",
+        skills: ["HTML", "CSS", "SASS"],
+      },
+    ],
+  };
+}
